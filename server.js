8739651d783600be5b9155ed93d83eadfa07a9f0
rachit <rachit345@yahoo.com>
2020-06-22 20:59:33 +0530

    integrated wit.ai

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
 const bodyParser = require('body-parser')
+const fetch = require('node-fetch')
 
 const accountSid = 'AC31b47c864d200ec38454dca109f2b67d';
 const authToken = '9253c5881b929c6f0e3e0d857a7e19bd';
 const client = require('twilio')(accountSid, authToken);
+
+const uri = 'https://api.wit.ai/message?q='
+CLIENT_TOKEN = 'GXSOHQRNFTVBGTORX5QXQJXJ3WXEARZS'
+const auth = 'Bearer ' + CLIENT_TOKEN
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -21,9 +26,10 @@ app.get("/", (req, res) =>{
 
 
 app.post("/get-msg", (req, res) => {
-
     const { From, Body } = req.body;
-    sendMsg("Got it", "whatsapp:+917042971742" )
+    uri = uri + encodedURIComponent(Body)
+    fetch(uri, {headers: {Authorization: auth}}).then(res => res.json()).then(res => console.log(JSON.stringify(res)))
+    sendMsg("Great! So you want to build an ecommerce website. What kind of business do you have ? ", From )
    
 
 })
@@ -40,7 +46,7 @@ function sendMsg(msg, number) {
         });
 }
 
-sendMsg("Henloooooo, this is website builder bot. I assume you want a website build for your business since you're already here. Let's begin by getting to know you better. Do you want to advertise your business or sell your products ?", "whatsapp:+917042971742")
+sendMsg("Henloooooo, this is website builder bot. I assume you want a website built for your business since you're already here. Let's begin by getting to know you better. Do you want to advertise your business or sell your products ?", "whatsapp:+917042971742")
 
 app.listen(port, ()=>{
     console.log("Server is running on port " + port);
