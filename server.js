209f1d74387628715aa247f8313f15e3d35f7ca8
rachit <rachit345@yahoo.com>
2020-06-23 17:06:00 +0530

    Update

--- server.js
+++ server.js
@@ -62,6 +62,7 @@ app.post("/get-msg", (req, res) => {
     }
     else if(!(users[From].siteCreated)){
         var lastQuery = users[From].lastQuery 
+        console.log("RECEIVED MESSAGE : " + Body)
         uri = uri + encodeURIComponent(Body)
         fetch(uri, {headers: {Authorization: auth}}).then(res => res.json()).then((res) => {
             if(lastQuery == "typeOfSite" || lastQuery == "typeOfBusiness"){
@@ -70,7 +71,7 @@ app.post("/get-msg", (req, res) => {
                 var value = res.entities[entity][0].value
                 users[From].data[lastQuery] = value
                 console.log("RESPONSE begin" + " " + lastQuery)
-                console.log(res)
+                console.log(value)
             }
             else{
                 users[From].data[lastQuery] = Body
